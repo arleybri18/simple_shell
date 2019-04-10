862fd3a233ce16dacecf98a3ae670857f6f4c4d7
arleybri18 <arleybri18@gmail.com>
2019-04-10 16:19:59 +0000

    add main program to shell and functions getline and exec

---
+++ shell.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - main function of shell
+ *
+ * Return - Always 0 if successful
+ */
+
+int main (void)
+{
+	while (1)
+	{
+		fun_getline();
+	}
+	return (0);
+}
