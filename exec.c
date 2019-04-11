f76c489493c38d8901a34687b63888accbd91446
RodrigoSierraV <735@holbertonschool.com>
2019-04-11 01:44:54 +0000

    Function exec handling commands like 'ls'

--- exec.c
+++ exec.c
@@ -6,14 +6,14 @@
  * @commands: contain the strings with the commands
  * Return: Nothing
  */
-
+char *_strcat(char *dest, char *src);
 void fun_exec(char **commands)
 {
 	int new_id;
 	struct stat st;
 	pid_t child_id;
 	path *head;
-	char *pathstr;
+	char *pathstr, *alone, bin[10] = "/bin/";
 	path **store_paths;
 
 	head = NULL;
@@ -26,6 +26,11 @@ void fun_exec(char **commands)
 	new_id = fork();
 	if (new_id == 0)
 	{
+		if (commands[0][0] != '/')
+		{
+			alone = commands[0];
+			commands[0] = strcat(bin, alone);
+		}
 		/*execute process*/
 		child_id = getpid();
 		if (stat(commands[0], &st) == 0)
@@ -43,3 +48,31 @@ void fun_exec(char **commands)
 		wait(&child_id);
 	}
 }
+	char *_strcat(char *dest, char *src)
+	{
+		int destlen = 0;
+		int srclen = 0;
+		int i = 0;
+		char b;
+
+		while (*(dest + destlen) != '\0')
+		{
+			destlen++;
+		}
+		while (*(src + srclen) != '\0')
+		{
+			srclen++;
+		}
+		for (i = 0; i < srclen; i++)
+		{
+			if (*(src + i) != '\0')
+			{
+				b = destlen + i;
+				*(dest + b) = *(src + i);
+			}
+		}
+		b = destlen + srclen;
+		*(dest + b) = '\0';
+		printf("Dest;%s", dest);
+		return (dest);
+	}
