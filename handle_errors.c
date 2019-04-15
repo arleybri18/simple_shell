8f3af8a3236082cc30bdf306a370360b9aef0bf4
arleybri18 <arleybri18@gmail.com>
2019-04-15 17:24:44 +0000

    changes handle_errors and print_env
    changes for don't use printfand manage errors and print enviroment with write, modify main function to receive parameters and send arguments and enviroment variable to the functions

---
+++ handle_errors.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * handle_errors - manage errors to printl
+ *
+ * @argv: arguments of main
+ * @commands: commands to print
+ * Return: Nothing
+ */
+
+void handle_errors(char **argv, char **commands)
+{
+	char *error = NULL;
+
+	error = _strcat(argv[0], ": 1: ");
+	error = _strcat(error, commands[0]);
+	error = _strcat(error, ": No such file or directory\n");
+	write(STDOUT_FILENO, error, _strlen(error));
+}
